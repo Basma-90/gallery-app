e33b3a8f0c0ecf6571c66d95fea48d012b7f08c3
Basma Sabry <basmasabry33333@gmail.com>
2024-04-19 19:56:23 +0200

    feat: Implement photo display and update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import path from "path";
 import config from "./config";
+import bodyParser from 'body-parser';
 
 import indexRouter from './routes/index.route';
+import uploadRouter from './routes/upload.route';
 
 const app = config.app;
 
@@ -10,7 +12,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'assets')));
+app.use('/images/uploads', express.static(path.join(__dirname, 'images', 'uploads')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', indexRouter);
+app.use('/upload', uploadRouter);
 
 config.runApp();
